160a65cdabbf8a9bff8126fc32f93a334d34e3b8
quangcaptain26-3 <phamminhquang2603@gmail.com>
2024-12-03 07:29:37 +0700

    Add project files.

---
+++ tính giá thuê phòng trọ/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace tính_giá_thuê_phòng_trọ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttontinhgiaphong = new System.Windows.Forms.Button();
+            this.buttontinhgiadv = new System.Windows.Forms.Button();
+            this.buttonthoat = new System.Windows.Forms.Button();
+            this.tboxsophong = new System.Windows.Forms.TextBox();
+            this.tboxgiaphong = new System.Windows.Forms.TextBox();
+            this.tboxtonggiaphong = new System.Windows.Forms.TextBox();
+            this.tboxgiadichvu = new System.Windows.Forms.TextBox();
+            this.tboxloaiphong = new System.Windows.Forms.TextBox();
+            this.buttonrs = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(162, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Số phòng";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(162, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 16);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Giá phòng";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(162, 253);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Giá phòng";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(630, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 16);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Loại phòng";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(630, 253);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 16);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Giá dịch vụ";
+            // 
+            // buttontinhgiaphong
+            // 
+            this.buttontinhgiaphong.Location = new System.Drawing.Point(165, 412);
+            this.buttontinhgiaphong.Name = "buttontinhgiaphong";
+            this.buttontinhgiaphong.Size = new System.Drawing.Size(140, 23);
+            this.buttontinhgiaphong.TabIndex = 1;
+            this.buttontinhgiaphong.Text = "Tính giá phòng";
+            this.buttontinhgiaphong.UseVisualStyleBackColor = true;
+            this.buttontinhgiaphong.Click += new System.EventHandler(this.buttontinhgiaphong_Click);
+            // 
+            // buttontinhgiadv
+            // 
+            this.buttontinhgiadv.Location = new System.Drawing.Point(482, 412);
+            this.buttontinhgiadv.Name = "buttontinhgiadv";
+            this.buttontinhgiadv.Size = new System.Drawing.Size(124, 23);
+            this.buttontinhgiadv.TabIndex = 1;
+            this.buttontinhgiadv.Text = "Tính giá dịch vụ";
+            this.buttontinhgiadv.UseVisualStyleBackColor = true;
+            this.buttontinhgiadv.Click += new System.EventHandler(this.buttontinhgiadv_Click);
+            // 
+            // buttonthoat
+            // 
+            this.buttonthoat.Location = new System.Drawing.Point(844, 412);
+            this.buttonthoat.Name = "buttonthoat";
+            this.buttonthoat.Size = new System.Drawing.Size(120, 23);
+            this.buttonthoat.TabIndex = 1;
+            this.buttonthoat.Text = "Thoát";
+            this.buttonthoat.UseVisualStyleBackColor = true;
+            this.buttonthoat.Click += new System.EventHandler(this.buttonthoat_Click);
+            // 
+            // tboxsophong
+            // 
+            this.tboxsophong.Location = new System.Drawing.Point(308, 98);
+            this.tboxsophong.Name = "tboxsophong";
+            this.tboxsophong.Size = new System.Drawing.Size(100, 22);
+            this.tboxsophong.TabIndex = 2;
+            // 
+            // tboxgiaphong
+            // 
+            this.tboxgiaphong.Location = new System.Drawing.Point(306, 176);
+            this.tboxgiaphong.Name = "tboxgiaphong";
+            this.tboxgiaphong.Size = new System.Drawing.Size(101, 22);
+            this.tboxgiaphong.TabIndex = 3;
+            // 
+            // tboxtonggiaphong
+            // 
+            this.tboxtonggiaphong.Location = new System.Drawing.Point(308, 247);
+            this.tboxtonggiaphong.Name = "tboxtonggiaphong";
+            this.tboxtonggiaphong.Size = new System.Drawing.Size(101, 22);
+            this.tboxtonggiaphong.TabIndex = 3;
+            // 
+            // tboxgiadichvu
+            // 
+            this.tboxgiadichvu.Location = new System.Drawing.Point(722, 247);
+            this.tboxgiadichvu.Name = "tboxgiadichvu";
+            this.tboxgiadichvu.Size = new System.Drawing.Size(101, 22);
+            this.tboxgiadichvu.TabIndex = 3;
+            // 
+            // tboxloaiphong
+            // 
+            this.tboxloaiphong.Location = new System.Drawing.Point(722, 92);
+            this.tboxloaiphong.Name = "tboxloaiphong";
+            this.tboxloaiphong.Size = new System.Drawing.Size(101, 22);
+            this.tboxloaiphong.TabIndex = 3;
+            // 
+            // buttonrs
+            // 
+            this.buttonrs.Location = new System.Drawing.Point(1140, 412);
+            this.buttonrs.Name = "buttonrs";
+            this.buttonrs.Size = new System.Drawing.Size(75, 23);
+            this.buttonrs.TabIndex = 4;
+            this.buttonrs.Text = "Reset";
+            this.buttonrs.UseVisualStyleBackColor = true;
+            this.buttonrs.Click += new System.EventHandler(this.buttonrs_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1329, 566);
+            this.Controls.Add(this.buttonrs);
+            this.Controls.Add(this.tboxloaiphong);
+            this.Controls.Add(this.tboxgiadichvu);
+            this.Controls.Add(this.tboxtonggiaphong);
+            this.Controls.Add(this.tboxgiaphong);
+            this.Controls.Add(this.tboxsophong);
+            this.Controls.Add(this.buttonthoat);
+            this.Controls.Add(this.buttontinhgiadv);
+            this.Controls.Add(this.buttontinhgiaphong);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttontinhgiaphong;
+        private System.Windows.Forms.Button buttontinhgiadv;
+        private System.Windows.Forms.Button buttonthoat;
+        private System.Windows.Forms.TextBox tboxsophong;
+        private System.Windows.Forms.TextBox tboxgiaphong;
+        private System.Windows.Forms.TextBox tboxtonggiaphong;
+        private System.Windows.Forms.TextBox tboxgiadichvu;
+        private System.Windows.Forms.TextBox tboxloaiphong;
+        private System.Windows.Forms.Button buttonrs;
+    }
+}
+
---
+++ tính giá thuê phòng trọ/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tính_giá_thuê_phòng_trọ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonthoat_Click(object sender, EventArgs e)
+        {
+            DialogResult thongbao;
+            thongbao = (MessageBox.Show("Bạn có muốn thoát ra không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Warning));
+            if(thongbao == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void buttontinhgiaphong_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            if(tboxgiaphong != null)
+            {
+                a=int.Parse(tboxgiaphong.Text);
+            }
+            else { a=0; };
+            if(tboxloaiphong != null)
+            {
+                b = int.Parse(tboxloaiphong.Text);
+            }
+            else { b =0; };
+            if (b == 1)
+            {
+                tboxtonggiaphong.Text =a.ToString();
+            }
+            if (b == 2)
+            {
+                tboxtonggiaphong.Text=(a*1.5).ToString();
+            }
+            if(b == 3)
+            {
+                tboxtonggiaphong.Text=(a*2).ToString();
+            };
+            
+        }
+
+        private void buttontinhgiadv_Click(object sender, EventArgs e)
+        {
+            double a, b,c;
+            if (tboxgiaphong != null)
+            {
+                a = int.Parse(tboxgiaphong.Text);
+            }
+            else { a = 0; };
+            if (tboxloaiphong != null)
+            {
+                b = int.Parse(tboxloaiphong.Text);
+            }
+            else { b = 0; };
+            if (b == 1)
+            {
+                tboxtonggiaphong.Text = a.ToString();
+            }
+            if (b == 2)
+            {
+                tboxtonggiaphong.Text = (a * 1.5).ToString();
+            }
+            if (b == 3)
+            {
+                tboxtonggiaphong.Text = (a * 2).ToString();
+            };
+            c = int.Parse(tboxtonggiaphong.Text);
+            tboxgiadichvu.Text = (c*0.1).ToString();
+        }
+
+        private void buttonrs_Click(object sender, EventArgs e)
+        {
+            tboxsophong.ResetText();
+            tboxgiaphong.ResetText();
+            tboxloaiphong.ResetText();
+            tboxgiadichvu.ResetText();
+            tboxtonggiaphong.ResetText();
+        }
+    }
+}
